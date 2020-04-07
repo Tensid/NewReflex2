ee1c3218ea854367dd383adba05d31c08fe580cb
misv <mischa.svensson@sokigo.com>
2020-04-07 10:57:24 +0200

    Lägg till karta

--- Reflex/ClientApp/src/Map.tsx
+++ Reflex/ClientApp/src/Map.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import OlMap from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import 'ol/ol.css';
+import OSM from 'ol/source/OSM';
+import React, { useEffect, useRef } from 'react';
 
-const Map = () => (
-  <h4>Karta</h4>
-);
+const Map = () => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    new OlMap({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: mapContainer.current!,
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }, []);
+
+  return (
+    <div id="map" ref={mapContainer} style={{ height: '720px' }} />
+  );
+};
 
 export default Map;
