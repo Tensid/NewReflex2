e2cccd8aefe03ff1a763df01d961b4701cd30244
misv <mischa.svensson@sokigo.com>
2021-05-21 17:14:51 +0200

    Nytt UI för sortering och filtrering på ärenden i ärendevy

--- Reflex/ClientApp/src/Cases.tsx
+++ Reflex/ClientApp/src/Cases.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import BootstrapSwitchButton from 'bootstrap-switch-button-react';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import _ from 'lodash';
+import { ButtonGroup, Col, Dropdown, DropdownButton, Row } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { Case, CaseSource, SearchResult } from './api/api';
 import { RootState } from './app/store';
@@ -19,21 +22,70 @@ export interface ModalData {
   title: string;
 }
 
+type SortBy = 'TITLE_ASC' | 'TITLE_DESC' | 'DATE_ASC' | 'DATE_DESC' | 'DNR_ASC' | 'DNR_DESC';
+
+function sortCases(filteredCases: Case[], sort: SortBy | undefined) {
+  switch (sort) {
+    case 'TITLE_ASC':
+      return filteredCases.sort((a, b) => a.title.localeCompare(b.title));
+    case 'TITLE_DESC':
+      return filteredCases.sort((a, b) => b.title.localeCompare(a.title));
+    case 'DATE_ASC':
+      return filteredCases.sort((a, b) => {
+        if (new Date(a.date) > new Date(b.date))
+          return 1;
+        if (new Date(a.date) < new Date(b.date))
+          return -1;
+        return 0;
+      });
+    case 'DATE_DESC':
+      return filteredCases.sort((a, b) => {
+        if (new Date(a.date) > new Date(b.date))
+          return -1;
+        if (new Date(a.date) < new Date(b.date))
+          return 1;
+        return 0;
+      });
+    case 'DNR_ASC':
+      return filteredCases.sort((a, b) => a.dnr.localeCompare(b.dnr));
+    case 'DNR_DESC':
+      return filteredCases.sort((a, b) => b.dnr.localeCompare(a.dnr));
+    default:
+      return filteredCases;
+  }
+}
+
 const Cases = ({ cases, searchResult, loading }: CasesProps) => {
-  const [showAgs, setShowAgs] = useState(true);
-  const [showByggr, setShowByggr] = useState(true);
-  const [showEcos, setShowEcos] = useState(true);
   const [filteredCases, setFilteredCases] = useState<Case[]>(cases);
+  const [sortBy, setSortBy] = useState<SortBy>();
+  const [showStatus, setShowStatus] = useState(false);
+  const [showCaseSource, setShowCaseSource] = useState(false);
+  const [caseSourceFilter, setCaseSourceFilter] = useState<string[]>([]);
+  const [statusFilter, setStatusFilter] = useState<string[]>([]);
   const [show, setShow] = useState(false);
   const toggleShow = () => setShow(!show);
   const [modalData, setModalData] = useState<ModalData>();
+  const caseSources: CaseSource[] = [];
+  const statuses: string[] = [];
+  cases.forEach(c => {
+    if (!caseSources.includes(c.caseSource))
+      caseSources.push(c.caseSource);
+    if (c?.status && !statuses.includes(c.status))
+      statuses.push(c.status);
+  });
+
+  const handleCaseSourceFilter = (caseSource: CaseSource) => {
+    setCaseSourceFilter(_.xor(caseSourceFilter, [caseSource]));
+  };
+
+  const handleStatusFilter = (status: string) => {
+    setStatusFilter(_.xor(statusFilter, [status]));
+  };
 
   useEffect(() => {
-    setFilteredCases(cases.filter(x =>
-      (showAgs && x.caseSource === CaseSource.AGS)
-      || (showByggr && x.caseSource === CaseSource.ByggR)
-      || (showEcos && x.caseSource === CaseSource.Ecos)));
-  }, [showAgs, showByggr, showEcos, cases]);
+    const sortedCases = cases.filter(x => (caseSourceFilter.length === 0 || caseSourceFilter.includes(x.caseSource)) && (statusFilter.length === 0 || statusFilter.includes(x.status)));
+    setFilteredCases(sortCases(sortedCases, sortBy));
+  }, [cases, caseSourceFilter, statusFilter, sortBy]);
 
   return (
     <>
@@ -44,14 +96,55 @@ const Cases = ({ cases, searchResult, loading }: CasesProps) => {
           'Adress': <div>Ärenden för: {searchResult.displayName}</div>
         }[searchResult.type!] || <div>Inget objekt valt</div>}
       </h5>
-      {cases?.length > 0 && <>
-        <BootstrapSwitchButton onlabel="Ecos" offlabel="Ecos" width={77} onstyle="success" style="mr-2" checked={showEcos}
-          onChange={() => setShowEcos(!showEcos)} />
-        <BootstrapSwitchButton onlabel="ByggR" offlabel="ByggR" width={77} onstyle="info" style="mr-2" checked={showByggr}
-          onChange={() => setShowByggr(!showByggr)} />
-        <BootstrapSwitchButton onlabel="AGS" offlabel="AGS" width={77} onstyle="warning" checked={showAgs}
-          onChange={() => setShowAgs(!showAgs)} />
-      </>}
+      {cases?.length > 0 &&
+        <Row>
+          <Col>
+            <ButtonGroup>
+              <DropdownButton variant="outline-secondary"
+                title={<>Ärendekällor {(caseSourceFilter.length > 0) && <span className="badge badge-secondary">{caseSourceFilter.length}</span>}</>}
+                className="pr-2" show={showCaseSource} id="caseSourceFilter"
+                onToggle={((show, e) => {
+                  if (!show && !e)
+                    setShowCaseSource(false);
+                  /*@ts-ignore*/
+                  if (e.target?.id === 'caseSourceFilter')
+                    setShowCaseSource(!showCaseSource);
+                })}
+              >
+                <Dropdown.Item disabled={!caseSources.includes(CaseSource.Ecos)} className="d-flex justify-content-between" onClick={() => handleCaseSourceFilter(CaseSource.Ecos)}>Ecos {caseSourceFilter.includes(CaseSource.Ecos) && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item disabled={!caseSources.includes(CaseSource.ByggR)} className="d-flex justify-content-between" onClick={() => handleCaseSourceFilter(CaseSource.ByggR)}>ByggR {caseSourceFilter.includes(CaseSource.ByggR) && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item disabled={!caseSources.includes(CaseSource.AGS)} className="d-flex justify-content-between" onClick={() => handleCaseSourceFilter(CaseSource.AGS)}>AGS {caseSourceFilter.includes(CaseSource.AGS) && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+              </DropdownButton>
+              {statuses.length > 0 && <DropdownButton variant="outline-secondary"
+                title={<>Status {(statusFilter.length > 0) && <span className="badge badge-secondary">{statusFilter.length}</span>}</>}
+                className="pr-2" show={showStatus} id="statusFilter"
+                onToggle={((show, e) => {
+                  if (!show && !e)
+                    setShowStatus(false);
+                  /*@ts-ignore*/
+                  if (e.target?.id === 'statusFilter')
+                    setShowStatus(!showStatus);
+                })}
+              >
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => handleStatusFilter('Pågående')}>Pågående {statusFilter.includes('Pågående') && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => handleStatusFilter('Avslutat')}>Avslutat {statusFilter.includes('Avslutat') && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+              </DropdownButton>}
+            </ButtonGroup>
+          </Col>
+          <Col className="text-right">
+            <ButtonGroup>
+              <DropdownButton title="Sortera" variant="outline-secondary">
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => (setSortBy('TITLE_ASC'))} >Titel stigande {sortBy === 'TITLE_ASC' && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => (setSortBy('TITLE_DESC'))} >Titel fallande {sortBy === 'TITLE_DESC' && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => (setSortBy('DATE_ASC'))} >Datum stigande {sortBy === 'DATE_ASC' && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => (setSortBy('DATE_DESC'))} >Datum fallande {sortBy === 'DATE_DESC' && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => (setSortBy('DNR_ASC'))} >DNR stigande {sortBy === 'DNR_ASC' && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+                <Dropdown.Item className="d-flex justify-content-between" onClick={() => (setSortBy('DNR_DESC'))} >DNR fallande {sortBy === 'DNR_DESC' && <FontAwesomeIcon icon={faCheck} />}</Dropdown.Item>
+              </DropdownButton>
+            </ButtonGroup>
+          </Col>
+        </Row>
+      }
       <div className="mt-2">
         {loading ? 'Hämtar ärenden...' :
           (filteredCases && filteredCases.length > 0) && <CaseList setModalData={setModalData} cases={filteredCases} toggleShow={toggleShow} />}
