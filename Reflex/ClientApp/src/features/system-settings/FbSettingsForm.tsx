1a741b549aa39ed402d36b9321acb369835450e8
misv <mischa.svensson@sokigo.com>
2022-11-16 17:11:21 +0100

    Avgränsa FB-inställningar

--- Reflex/ClientApp/src/features/system-settings/FbSettingsForm.tsx
+++ Reflex/ClientApp/src/features/system-settings/FbSettingsForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { updateFbSettings } from '../../api/settings/settingsApi';
 import PasswordInput from '../common/forms/PasswordInput';
@@ -34,9 +34,10 @@ const FbSettingsForm = ({ formData, setActiveKey }: FbSettingsFormProps) => {
         <TextInput name="fbWebbFastighetUsrUrl" label="FB Webb Fastighetsrapport med val URL" register={register} />
         <TextInput name="fbWebbByggnadUrl" label="FB Webb Byggnadsrapport URL" register={register} />
         <TextInput name="fbWebbByggnadUsrUrl" label="FB Webb Byggnadsrapport med val URL" register={register} />
+        <hr />
         <TextInput name="fbServiceUrl" label="FB Service URL" register={register} />
         <TextInput name="fbServiceDatabase" label="FB Databasnamn" register={register} />
-        <TextInput name="fbServiceUser" label="Användarnamn" register={register} />
+        <TextInput name="fbServiceUser" label="FB Användarnamn" register={register} />
         <PasswordInput name="fbServicePassword" label="FB Lösenord" register={register} />
         <button className="btn btn-primary" type="submit">Spara</button>
       </form>
